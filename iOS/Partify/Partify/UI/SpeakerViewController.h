1b2514ff9cf92ca012fbd0fe9d33eec23c5bbddc
Matt Malone <mmalone@roishoppermarketing.com>
2014-10-25 16:18:00 +0100

    More VCs

---
+++ iOS/Partify/Partify/UI/SpeakerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpeakerViewController.h
+//  Partify
+//
+//  Created by Matt Malone on 25/10/2014.
+//  Copyright (c) 2014 Apadmi Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpeakerViewController : UIViewController
+
+@end
