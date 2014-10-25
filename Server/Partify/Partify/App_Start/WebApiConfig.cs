6c35b1c9b8e843eb5f75f749fb564aa9672cacf8
RichHoare <Richhoare89@gmail.com>
2014-10-25 19:49:45 +0100

    json
    bang bang

--- Server/Partify/Partify/App_Start/WebApiConfig.cs
+++ Server/Partify/Partify/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Partify
@@ -14,6 +15,8 @@ namespace Partify
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
