bd9214e0e437ec399991954c2d1f067c41c334ea
Matt Malone <mmalone@roishoppermarketing.com>
2014-10-26 11:25:30 +0000

    Song info. Colours and logos

--- iOS/Partify/Partify/PlaylistProvider.m
+++ iOS/Partify/Partify/PlaylistProvider.m
@@ -7,16 +7,36 @@
 //
 
 #import "PlaylistProvider.h"
+#import "ServerManager.h"
+#import "AppDelegate.h"
 
 @interface PlaylistProvider ()
-
+@property (nonatomic, weak) AppDelegate *appD;
+@property (nonatomic, weak) ServerManager *serverManager;
 @end
 
 @implementation PlaylistProvider
+
+- (instancetype) init
+{
+    if ((self = [super init]))
+    {
+        _appD = [UIApplication sharedApplication].delegate;
+        _serverManager = _appD.serverManager;
+    }
+    return self;
+}
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"rrererere"];
-    cell.textLabel.text = self.playlist[indexPath.row];
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"rrererere"];
+    cell.backgroundColor = [UIColor colorWithRed:82.0f / 255.0f green:82.0f / 255.0f blue:82.0f / 255.0f alpha:1.0f];
+    
+    [self.serverManager lookupSongID:self.playlist[indexPath.row] withSuccessBlock:^(NSDictionary *songDetail) {
+        cell.textLabel.text = songDetail[@"name"];
+        cell.detailTextLabel.text = songDetail[@"artists"][0][@"name"];
+    } andFailureBlock:NULL];
+    
+    
     return cell;
 }
 
